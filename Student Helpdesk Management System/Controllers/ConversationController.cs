b61610c5096ba7a08c846f843fac0b9a3ba7f7e3
Muhammad Ibrahim <smdibrahim38@gmail.com>
2026-01-04 14:54:34 +0600

    added Controllers/ConversationController.cs

---
+++ Student Helpdesk Management System/Controllers/ConversationController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Student_Complain_Management_System.Controllers
+{
+    public class ConversationController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
