e043e6db81ec134e28932443650cb1654bc5701d
Muhammad Ibrahim <smdibrahim38@gmail.com>
2026-01-05 13:37:36 +0600

    Added Public Notice Page

---
+++ Student Helpdesk Management System/Controllers/PublicNoticeController.cs
@@ -0,0 +1,23 @@
+﻿using HelpdeskService.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Student_Complain_Management_System.Controllers
+{
+    [AllowAnonymous]
+    public class PublicNoticeController : Controller
+    {
+        private readonly INoticeService _noticeService;
+
+        public PublicNoticeController(INoticeService noticeService)
+        {
+            _noticeService = noticeService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var notices = await _noticeService.GetAllApprovedAsync();
+            return View(notices);
+        }
+    }
+}
